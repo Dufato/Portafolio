6dc4a50548c4c08910b71d37e417d04b2d3f887f
Dufato <dufatozdreyfus@gmail.com>
2025-07-21 16:45:37 -0400

    Primera subida de mi portafolio

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+ScrollReveal().reveal('.hero h1', { delay: 200, origin: 'left', distance: '50px' });
+ScrollReveal().reveal('.hero p', { delay: 400, origin: 'right', distance: '50px' });
+ScrollReveal().reveal('.btn', { delay: 600, origin: 'bottom', distance: '20px' });
